7127256d574f39b1e6cc6b71a6ad192c1bd81e1f
YuKun Guo <gyk8181219@outlook.com>
2019-12-02 14:29:05 +0800

    new

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,51 +1,46 @@
-/*
-	Halcyonic by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+$(function () {
+  // Cache variables for increased performance on devices with slow CPUs.
+  var flexContainer = $('div.flex-container')
+  var searchBox = $('.search-box')
+  var searchClose = $('.search-icon-close')
+  var searchInput = $('#search-input')
 
-(function($) {
+  // Menu Settings
+  $('.menu-icon, .menu-icon-close').click(function (e) {
+    e.preventDefault()
+    e.stopPropagation()
+    flexContainer.toggleClass('active')
+  })
 
-	var $window = $(window),
-		$body = $('body');
+  // Click outside of menu to close it
+  flexContainer.click(function (e) {
+    if (flexContainer.hasClass('active') && e.target.tagName !== 'A') {
+      flexContainer.removeClass('active')
+    }
+  })
 
-	// Breakpoints.
-		breakpoints({
-			xlarge:  [ '1281px',  '1680px' ],
-			large:   [ '981px',   '1280px' ],
-			medium:  [ '737px',   '980px'  ],
-			small:   [ null,      '736px'  ]
-		});
+  // Press Escape key to close menu
+  $(window).keydown(function (e) {
+    if (e.key === 'Escape') {
+      if (flexContainer.hasClass('active')) {
+        flexContainer.removeClass('active')
+      } else if (searchBox.hasClass('search-active')) {
+        searchBox.removeClass('search-active')
+      }
+    }
+  })
 
-	// Nav.
+  // Search Settings
+  $('.search-icon').click(function (e) {
+    e.preventDefault()
+    searchBox.toggleClass('search-active')
+    searchInput.focus()
 
-		// Title Bar.
-			$(
-				'<div id="titleBar">' +
-					'<a href="#navPanel" class="toggle"></a>' +
-					'<span class="title">' + $('#logo').html() + '</span>' +
-				'</div>'
-			)
-				.appendTo($body);
-
-		// Panel.
-			$(
-				'<div id="navPanel">' +
-					'<nav>' +
-						$('#nav').navList() +
-					'</nav>' +
-				'</div>'
-			)
-				.appendTo($body)
-				.panel({
-					delay: 500,
-					hideOnClick: true,
-					hideOnSwipe: true,
-					resetScroll: true,
-					resetForms: true,
-					side: 'left',
-					target: $body,
-					visibleClass: 'navPanel-visible'
-				});
-
-})(jQuery);
+    if (searchBox.hasClass('search-active')) {
+      searchClose.click(function (e) {
+    		e.preventDefault()
+    		searchBox.removeClass('search-active')
+    	})
+    }
+  })
+})
